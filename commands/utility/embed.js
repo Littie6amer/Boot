547af458a2213d089a02a58334c4e319e411640a
Littie6amer <toby5.tc@gmail.com>
2022-02-07 20:08:59 +0000

    Restricting

--- commands/utility/embed.js
+++ commands/utility/embed.js
@@ -6,6 +6,7 @@ const command = module.exports = new Command()
 command
     .create(["embed"], "Create an embed")
     .setExecute(execute)
+    .setRestriction("DEV")
 
 function execute(toolbox) {
     const { message, client, args } = toolbox
