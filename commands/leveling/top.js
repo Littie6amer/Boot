3334c9e170bc56f2c90cc0138f44128a3d15729f
Littie6amer <toby5.tc@gmail.com>
2022-02-20 15:04:50 +0000

    Top now displays top messages too!

--- commands/leveling/top.js
+++ commands/leveling/top.js
@@ -12,29 +12,42 @@ command
 module.exports = command
 
 async function execute(toolbox) {
-    const { message } = toolbox
+    const { message, args, userGuildProfile } = toolbox
+    if (!args[0] || !["messages", "levels"].includes(args[0].toLowerCase())) {
+        const embed = await utils.embeds.simpleUsageEmbed(command, " [messages | levels]")
+        return message.reply({ embeds: [embed] })
+    }
     let users = (await guildProfileSc.find({
         guildId: message.guild.id,
     }))
-        .filter(u => message.guild.members.cache.get(u.userId) && u.leveling.xp)
-        .sort((a, b) => sortBy(a, b, "xp"))
+        .filter(u => message.guild.members.cache.get(u.userId) && u[args[0] == "levels" ? "leveling" : "activity"][args[0] == "levels" ? "xp" : "overall"])
+        .sort((a, b) => sortBy(a, b, args[0].toLowerCase()))
         .reverse()
+        .slice(0, 10)
 
     //const components = []
+    let { xp, level } = utils.leveling.parseXp(userGuildProfile.leveling.xp)
+    let yourRankDisplay = args[0].toLowerCase() == "levels" ? `${level ? `Level ${level}, ` : ""}${xp}XP` : `${userGuildProfile.activity.overall.messages} messages`
+    let rank = "00".slice(`${users.findIndex(u => u.userId == message.author.id)} + 1`.length) + users.findIndex(u => u.userId == message.author.id) + 1
     const embed = new MessageEmbed()
-        .setDescription(users.map(u => leaderboardPlace(message.guild.members.cache.get(u.userId).user, u, users.findIndex(i => i == u) + 1, "xp")).join("\n"))
-        .setColor("2f3136");
-    message.channel.send({
-        embeds: [embed]
-    })
+        .setAuthor({ name: message.guild.name, iconURL: message.guild.iconURL() })
+        .setDescription(
+            users.map(u =>
+                leaderboardPlace(message.guild.members.cache.get(u.userId).user, u, users.findIndex(i => i == u) + 1, args[0].toLowerCase())
+            )
+                .join("\n")
+        )
+        .addField("\u200b", `\`Rank #${rank}\`<:Blank1:801947188590411786>[${message.author.username}](https://discord.com/users/${message.author.id}) - ${yourRankDisplay}`)
+        .setColor("2f3136")
+    message.channel.send({ embeds: [embed] })
 }
 
-function sortBy (a, b, type) {
-    return  type == "xp" ? (a.leveling.xp - b.leveling.xp) : (a.activity.overall.messages - b.activity.overall.messages)
+function sortBy(a, b, type) {
+    return type == "levels" ? (a.leveling.xp - b.leveling.xp) : (a.activity.overall.messages - b.activity.overall.messages)
 }
 
 function leaderboardPlace(user, userData, rank, type) {
     let { xp, level } = utils.leveling.parseXp(userData.leveling.xp)
-    let display = type == "xp" ? `${level ? `Level ${level}, ` : ""}${xp}XP` : `${userData.activity.overall.messages} messages`
-    return `\`Rank #${rank}\`<:Blank1:801947188590411786>**${user.tag}** ${display}`
+    let display = type == "levels" ? `${level ? `Level ${level}, ` : ""}${xp}XP` : `${userData.activity.overall.messages} messages`
+    return `\`Rank #${"00".slice(rank.toString().length) + rank}\`<:Blank1:801947188590411786>[${user.username}](https://discord.com/users/${user.id}) - ${display}`
 }
