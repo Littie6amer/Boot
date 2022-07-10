5910d23ed216ff734f0e113e16dd7f7b051c712b
Littie6amer <toby5.tc@gmail.com>
2022-07-10 15:19:55 +0100

    Start from new.

--- index.js
+++
@@ -1,8 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const process_settings = require("./process-settings")
-
-const manager = new ShardingManager('./bot.js', { token: process_settings.botToken, totalShards: process_settings.shardCount, respawn: true });
-
-manager.on('shardCreate', shard => console.log(`[Shard Manager]: Launched shard ${shard.id}`));
-
-manager.spawn()
